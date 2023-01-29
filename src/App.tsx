9d1b8d78450e979e8f1fbf0bdfa3a4ba983acd6e
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-30 00:26:02 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+
+    <div> Vocabulary brother!</div>
+
+  )
+}
+
+export default App
