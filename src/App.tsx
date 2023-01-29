5774b04e37f26891630821199689a054a27b3e52
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-30 00:37:21 +0100

    Setting the React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
 
-    <div> Vocabulary brother!</div>
+    <Routes>
+       
+        <Route path='/Home' element={ <h1>Vocabulary brother</h1> } />
+        <Route path='/Me' element={ <h1>Me</h1> } />
+        <Route path='*' element={ <Navigate to={'/'} /> } />
+
+    </Routes>
+   
 
   )
 }
