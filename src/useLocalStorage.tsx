bc77f8cfdd6bc81535bfb34418395857c5ef823d
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-31 00:25:06 +0100

    local storage component add

---
+++ src/useLocalStorage.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+export default function useLocalStorage<T>(key: string, initialValue: T | ( () => T )) {
+
+  const [value, setValue] = useState<T>( () => {    
+
+    const localData = localStorage.getItem(key);
+
+    if (localData == null) {
+
+        if (typeof initialValue === 'function') {
+
+            return( initialValue as () => T)();
+
+        } else {
+            return initialValue;
+        }
+
+    } else {
+
+        return JSON.parse(localData);
+
+    }
+
+  } );  // check if the value changes
+
+
+  useEffect( ()=> {
+
+    localStorage.setItem( key, JSON.stringify(value) );
+
+  }, [value, key]);   // then update it
+
+
+  return [value, setValue] as [T, typeof setValue];
+
+}
