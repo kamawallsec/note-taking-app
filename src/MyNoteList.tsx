8f19a3223e967591d3977701175f9dc5d7bfba2d
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-01 00:01:04 +0100

    initial card

--- src/MyNoteList.tsx
+++ src/MyNoteList.tsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react'
-import { Button, Col, Row, Stack, Form } from 'react-bootstrap'
+import React, { useMemo, useState } from 'react'
+import { Button, Col, Row, Stack, Form, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import ReactSelect from 'react-select/creatable'
 import { Tag } from './App'
+import styles from './MyNoteList.module.css'
 
+
+type SimpNote = {
+
+    id: string,
+    title: string,
+    tags: Tag[],
+
+}
 
 type MyNoteListProps = {
 
     tagsAvailable: Tag[],
-
+    notes: SimpNote[],
 }
 
 
-const MyNoteList = ({ tagsAvailable }:MyNoteListProps ) => {
+
+const MyNoteList = ({ tagsAvailable, notes }:MyNoteListProps ) => {
 
 
     const [selectTags, setSelectTags] = useState<Tag[]>([]);
     const [title, setTitle] = useState('');
+    
+    const filterNotes = useMemo( ()=> {
+        
+        return notes.filter( note=> {
+            return (title === '' || note.title.toLowerCase().includes(title.toLowerCase())) && (selectTags.length === 0 || selectTags.every(tag => note.tags.some( noteTag => noteTag.id === tag.id ) ) )
+        } )
 
+    },  [title, selectTags, notes])
 
   return (
 
     <>
     
-        <Row>
+        <Row className='mb-4 align-items-center'>
 
             <Col>
                 <h1>Your Notes</h1>             
@@ -83,6 +100,18 @@ const MyNoteList = ({ tagsAvailable }:MyNoteListProps ) => {
 
         </Form>
 
+
+        <Row xs={1} sm={2} lg={3} xl={4} gap={4}>
+
+            {filterNotes.map(note => {
+                return <Col key={note.id}>
+                    <NoteCard id={note.id} title={note.title} tags={note.tags} />
+                </Col>
+            })}
+
+        </Row>
+
+
     </>
 
   )
@@ -90,3 +119,13 @@ const MyNoteList = ({ tagsAvailable }:MyNoteListProps ) => {
 
 export default MyNoteList
 
+
+const NoteCard = function({ id, title, tags }: SimpNote) {
+
+    return <Card as = {Link} to={`/${id}`} className={` h-100 text-reset text-decoration-none ${styles.cardStyle} `}>
+
+        <Card.Body></Card.Body>
+
+    </Card>
+
+}
