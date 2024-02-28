14d2dc97ef446ac33fa1a851cc3491b9263a3614
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2024-02-28 21:56:17 +0100

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,88 @@
-import { useMemo, useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import NewNote from './NewNote';
-import useLocalStorage from './useLocalStorage';
-import {v4 as uuidV4} from 'uuid';  
-import MyNoteList from './MyNoteList';
+import { useMemo, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NewNote from "./NewNote";
+import useLocalStorage from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
+import MyNoteList from "./MyNoteList";
 
+export type Note = {
+  id: string;
+} & NoteData; // id to the NoteData
 
-  export type Note = {
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
 
-    id: string,
+export type Tag = {
+  id: string;
+  label: string;
+};
 
-  } & NoteData     // id to the NoteData
+export type NoteRaw = {
+  id: string;
+} & NoteRawData;
 
-  export type NoteData = {
-
-    title: string,
-    markdown: string,
-    tags: Tag[],
-
-  }
-
-  export type Tag = {
-
-    id: string,
-    label: string,
-
-  }
-
-
-  export type NoteRaw = {
-
-    id: string,
-
-  } & NoteRawData
-
-  export type NoteRawData = {
-
-    title: string,
-    markdown: string,
-    tagId: string[],
-
-  }
-
-
+export type NoteRawData = {
+  title: string;
+  markdown: string;
+  tagId: string[];
+};
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<NoteRaw[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
-  const [notes, setNotes] = useLocalStorage<NoteRaw[]>('NOTES', []);
-  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', []);
+  const noteWithTag = useMemo(() => {
+    return notes.map((note) => {
+      return { ...note, tags: tags.filter((t) => note.tagId.includes(t.id)) };
+    });
+  }, [notes, tags]);
 
+  const CreateNote = function ({ tags, ...data }: NoteData) {
+    setNotes((previousNotes) => {
+      return [
+        ...previousNotes,
+        { ...data, id: uuidV4(), tagId: tags.map((tag) => tag.id) },
+      ];
+    });
+  };
 
-  const [noteWithTag, setNoteWithTag] = useMemo( () => {
-
-    return notes.map(note => {
-
-      return {...note, tags: tags.filter( t => note.tagId.includes(t.id))};
-
-    })
-
-  }, [notes, tags]);  
-
-
-  const CreateNote = function( { tags, ...data }: NoteData) {
-
-    setNotes( previousNotes => {
-
-      return [...previousNotes, {...data, id: uuidV4(), tagId: tags.map(tag => tag.id) } ]
-
-    } )
-
-  }
-
-  const AddTag = function(tag: Tag) {
-
-    setTags( previousTags => [...previousTags, tag]);
-
-  }
-
+  const AddTag = function (tag: Tag) {
+    setTags((previousTags) => [...previousTags, tag]);
+  };
 
   return (
-  
-      <Container className='my-4'>
+    <Container className="my-4">
+      <Routes>
+        <Route
+          path="/"
+          element={<MyNoteList notes={noteWithTag} tagsAvailable={tags} />}
+        />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={CreateNote}
+              onAddTag={AddTag}
+              tagsAvailable={tags}
+            />
+          }
+        />
 
-        <Routes>
-          
-            <Route path='/' element={ <MyNoteList tagsAvailable ={ tags } />   } />
-            <Route path='/new' element={ <NewNote onSubmit = {CreateNote} onAddTag = {AddTag} tagsAvailable = {tags} />} />
-            
-            <Route path='/:id'>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
 
-                <Route index element={<h1>Show</h1>} />
-                <Route path='edit' element={<h1>Edit</h1>} />
-
-            </Route>
-
-            <Route path='*' element={ <Navigate to='/' /> } />
-
-        </Routes>
-
-      </Container>
-
-  )
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/MyNoteList.tsx
+++ src/MyNoteList.tsx
@@ -1,142 +1,142 @@
-import React, { useMemo, useState } from 'react'
-import { Button, Col, Row, Stack, Form, Card, Badge } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import ReactSelect from 'react-select/creatable'
-import { Tag } from './App'
-import styles from './MyNoteList.module.css'
-
+import React, { useMemo, useState } from "react";
+import { Button, Col, Row, Stack, Form, Card, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ReactSelect from "react-select/creatable";
+import { Tag } from "./App";
+import styles from "./MyNoteList.module.css";
 
 type SimpNote = {
-
-    id: string,
-    title: string,
-    tags: Tag[],
-
-}
+  id: string;
+  title: string;
+  tags: Tag[];
+};
 
 type MyNoteListProps = {
+  tagsAvailable: Tag[];
+  notes: SimpNote[];
+};
 
-    tagsAvailable: Tag[],
-    notes: SimpNote[],
-}
+const MyNoteList = ({ tagsAvailable, notes }: MyNoteListProps) => {
+  const [selectTags, setSelectTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
 
-
-
-const MyNoteList = ({ tagsAvailable, notes }:MyNoteListProps ) => {
-
-
-    const [selectTags, setSelectTags] = useState<Tag[]>([]);
-    const [title, setTitle] = useState('');
-    
-    const filterNotes = useMemo( ()=> {
-        
-        return notes.filter( note=> {
-            return (title === '' || note.title.toLowerCase().includes(title.toLowerCase())) && (selectTags.length === 0 || selectTags.every(tag => note.tags.some( noteTag => noteTag.id === tag.id ) ) )
-        } )
-
-    },  [title, selectTags, notes])
+  const filterNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectTags.length === 0 ||
+          selectTags.every((tag) =>
+            note.tags.some((noteTag) => noteTag.id === tag.id)
+          ))
+      );
+    });
+  }, [title, selectTags, notes]);
 
   return (
-
     <>
-    
-        <Row className='mb-4 align-items-center'>
+      <Row className="mb-4 align-items-center">
+        <Col>
+          <h1>Your Notes</h1>
+        </Col>
 
-            <Col>
-                <h1>Your Notes</h1>             
-            </Col>
+        <Col xs="auto">
+          <Stack gap={4} direction="horizontal">
+            <Link to="/new">
+              <Button type="submit" variant="warning">
+                Create
+              </Button>
+            </Link>
 
-            <Col xs='auto'>
-                <Stack gap={4} direction='horizontal'>
-                    <Link to='/new'>
-                        <Button type='submit' variant='warning'>Create</Button>
-                    </Link>
-                    
-                    <Button type='button' variant='light'>Edit Tags</Button>
-                </Stack>
-            </Col>
+            <Button type="button" variant="light">
+              Edit Tags
+            </Button>
+          </Stack>
+        </Col>
+      </Row>
 
+      <Form>
+        <Row className="mb-4">
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                className="bg-dark text-light"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Note Tags</Form.Label>
+
+              <ReactSelect
+                value={selectTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value }; // convert or set tags to label and id
+                    })
+                  );
+                }}
+                options={tagsAvailable.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
         </Row>
+      </Form>
 
-
-        <Form>
-
-            <Row className='mb-4'>
-
-                <Col>
-                    <Form.Group controlId='title'>
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type='text' className='bg-dark text-light' value={title} 
-                        onChange={ e=> setTitle(e.target.value) }
-                        />
-                    </Form.Group>
-                </Col>
-
-                <Col>
-                
-                    <Form.Group controlId='tags' >
-
-                        <Form.Label>Note Tags</Form.Label>
-
-                        <ReactSelect 
-                        
-                            value={selectTags.map((tag) => {
-                                return {label: tag.label, value: tag.id}
-                        })}  onChange={tags => {
-                            setSelectTags(tags.map((tag) => {
-                                return {label: tag.label, id: tag.value}     // convert or set tags to label and id
-                            }))
-                        }}
-                        options = { tagsAvailable.map((tag) =>  {
-                            return {label: tag.label, value: tag.id}
-                        })}
-                        isMulti />
-
-                        </Form.Group>
-
-                </Col>
-
-            </Row>
-
-        </Form>
-
-
-        <Row xs={1} sm={2} lg={3} xl={4} className='g-4'>
-
-            {filterNotes.map(note => {
-                return <Col key={note.id}>
-                    <NoteCard id={note.id} title={note.title} tags={note.tags} />
-                </Col>
-            })}
-
-        </Row>
-
-
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-4">
+        {filterNotes.map((note) => {
+          return (
+            <Col key={note.id}>
+              <NoteCard id={note.id} title={note.title} tags={note.tags} />
+            </Col>
+          );
+        })}
+      </Row>
     </>
+  );
+};
 
-  )
-}
+export default MyNoteList;
 
-export default MyNoteList
-
-
-const NoteCard = function({ id, title, tags }: SimpNote) {
-
-    return <Card as = {Link} to={`/${id}`} className={` h-100 text-reset text-decoration-none ${styles.cardStyle} `}>
-
-        <Card.Body className='bg-dark text-light'>
-
-            <Stack gap={4} className='h-100 align-items-center justify-content-center'>
-                <span className='fs-4'>{title}</span>
-                {tags.length > 0 && (
-                    <Stack gap={2} direction='horizontal' className='justify-content-center flex-wrap'>
-                       
-                    </Stack>
-                )}
+const NoteCard = function ({ id, title, tags }: SimpNote) {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={` h-100 text-reset text-decoration-none ${styles.cardStyle} `}
+    >
+      <Card.Body className="bg-dark text-light">
+        <Stack
+          gap={4}
+          className="h-100 align-items-center justify-content-center"
+        >
+          <span className="fs-4">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={2}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
             </Stack>
-
-        </Card.Body>
-
+          )}
+        </Stack>
+      </Card.Body>
     </Card>
-
-}
+  );
+};
